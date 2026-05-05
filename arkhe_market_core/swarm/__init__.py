198bd103da826721ae7a0c449e814fd1a4692572
Brian Salsbury <brian.salsbury89@gmail.com>
2026-05-05 09:08:01 -0700

    Initial swarm module structure

--- arkhe_market_core/swarm/__init__.py
+++ arkhe_market_core/swarm/__init__.py
@@ -2,3 +2,8 @@
 # Distributed adaptive swarm intelligence layer
 
 from .swarm_logic import apply_local_override
+from .daily_report_generator import DailyReportGenerator
+from .swarm_client import SwarmClient
+from .swarm_coordinator import SwarmCoordinator
+
+__all__ = ['apply_local_override', 'DailyReportGenerator', 'SwarmClient', 'SwarmCoordinator']
